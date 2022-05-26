4c13f0f3c2725011547267fbf19f8ae4e32123ff
hwimin-kim <gnlals7841@gmail.com>
2022-05-26 23:09:19 +0900

    posts 수정/조회 api commit

--- src/main/java/com/example/demo/service/posts/PostsService.java
+++ src/main/java/com/example/demo/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.example.demo.service.posts;
 
+import com.example.demo.domain.posts.Posts;
 import com.example.demo.domain.posts.PostsRepository;
+import com.example.demo.web.dto.PostsResponseDto;
 import com.example.demo.web.dto.PostsSaveRequestDto;
+import com.example.demo.web.dto.PostsUpdateRequestDto;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +19,24 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntitiy()).getId();
+    }
+    
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                                    .orElseThrow(() -> new
+                                    IllegalArgumentException("해당  게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                                    .orElseThrow(()->new 
+                                    IllegalArgumentException("해당  게시글이 없습니다. id=" + id));
+        
+        return new PostsResponseDto(entity);
     }
     
 }
--- src/main/java/com/example/demo/web/PostsApiController.java
+++ src/main/java/com/example/demo/web/PostsApiController.java
@@ -1,9 +1,14 @@
 package com.example.demo.web;
 
 import com.example.demo.service.posts.PostsService;
+import com.example.demo.web.dto.PostsResponseDto;
 import com.example.demo.web.dto.PostsSaveRequestDto;
+import com.example.demo.web.dto.PostsUpdateRequestDto;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +23,16 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
