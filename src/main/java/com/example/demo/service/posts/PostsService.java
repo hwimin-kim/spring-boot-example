95fff35cfc4050849900a6841d6d010b259514fb
hwimin-kim <gnlals7841@gmail.com>
2022-06-02 18:20:52 +0900

    전체 조회 화면

--- src/main/java/com/example/demo/service/posts/PostsService.java
+++ src/main/java/com/example/demo/service/posts/PostsService.java
@@ -2,9 +2,13 @@ package com.example.demo.service.posts;
 
 import com.example.demo.domain.posts.Posts;
 import com.example.demo.domain.posts.PostsRepository;
+import com.example.demo.web.dto.PostsListResponseDto;
 import com.example.demo.web.dto.PostsResponseDto;
 import com.example.demo.web.dto.PostsSaveRequestDto;
 import com.example.demo.web.dto.PostsUpdateRequestDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,4 +43,10 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
     
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                                .map(PostsListResponseDto::new)
+                                .collect(Collectors.toList());
+    }
 }
