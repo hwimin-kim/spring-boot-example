72013a10d1498b73aec312c2f4e7eef594276497
hwimin-kim <gnlals7841@gmail.com>
2022-06-02 19:20:00 +0900

    게시글 수정 삭제

--- src/main/java/com/example/demo/web/IndexController.java
+++ src/main/java/com/example/demo/web/IndexController.java
@@ -2,9 +2,12 @@ package com.example.demo.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.service.posts.PostsService;
+import com.example.demo.web.dto.PostsResponseDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,5 +26,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        
+        return "posts-update";
     }
 }
