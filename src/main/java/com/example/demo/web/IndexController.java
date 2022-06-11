b27b26e5f3c85af40a3c1d90316c1857df4ec388
hwimin-kim <gnlals7841@gmail.com>
2022-06-12 01:07:10 +0900

    google 로그인 기능 추가 / 오류 해결: 도메인 -  User.java - @Entity(name="users") 추가

--- src/main/java/com/example/demo/web/IndexController.java
+++ src/main/java/com/example/demo/web/IndexController.java
@@ -1,11 +1,14 @@
 package com.example.demo.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.demo.config.auth.dto.SessionUser;
 import com.example.demo.service.posts.PostsService;
 import com.example.demo.web.dto.PostsResponseDto;
 
@@ -14,12 +17,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
-    
+
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -32,7 +40,7 @@ public class IndexController {
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
-        
+
         return "posts-update";
     }
 }
